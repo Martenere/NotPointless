a8c9017c5c6af6ddbc0eeaa9ad77838a6beb6869
Mårten Elfving <Marten.elfving@gmail.com>
2024-02-29 22:54:22 +0100

    Visual improvement to Login-pane

--- React/src/Components/LoginPane/LoginPane.jsx
+++ React/src/Components/LoginPane/LoginPane.jsx
@@ -2,7 +2,7 @@ import "./LoginPane.css";
 function LoginPane() {
   return (
     <div className="Modal-Login Login-Pane ">
-      <h1 className="mx-3 p-4">Login</h1>
+      <h1 className="h3-color mx-3 p-4">Login</h1>
       <div className="d-flex justify-content-center flex-column">
         <label className="d-flex justify-content-center align-items-center flex-column">
           <h2 className="mx-3">Name</h2>
